69e052d9fedf2710f1fd65c2c7a7c2b4e4a147bc
williamfisher9 <fisher.a.william@gmail.com>
2025-04-06 05:59:14 -0500

    updated cors configs

--- src/main/java/com/apps/biteandsip/security/SecurityConfigs.java
+++ src/main/java/com/apps/biteandsip/security/SecurityConfigs.java
@@ -53,7 +53,7 @@ public class SecurityConfigs {
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization","content-type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
